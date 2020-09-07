1ddab707e3775f6c63afa1c5580612ba383236dc
Timo Bechtel <hi@timobechtel.com>
2020-09-07 21:06:33 +0200

    fix: add NodeList typeguard

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,16 @@ export const accept = (input: UniversalElementSelector): HTMLElement[] => {
   if (typeof input === 'string')
     return Array.from(document.querySelectorAll(input));
   if (Array.isArray(input)) return input;
-  if ('length' in input) return Array.from(input) as HTMLElement[];
+  if ('length' in input)
+    return Array.from(input).reduce(nodeToElementArray, []);
   return [input];
+};
+
+// typeguard
+const nodeToElementArray = (
+  array: HTMLElement[],
+  node: Node
+): HTMLElement[] => {
+  if (node.nodeType === Node.ELEMENT_NODE) array.push(node as HTMLElement);
+  return array;
 };
