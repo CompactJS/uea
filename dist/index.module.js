d58c1bf5090959ec20c0e67b64f8b643b78af6fd
TimoBechtel <TimoBechtel@users.noreply.github.com>
2021-04-19 11:01:15 +0000

    deploy: 5f26190ea500e2bd82c9832a3f37614134388337

--- dist/index.module.js
+++
@@ -1,2 +0,0 @@
-var r=function(r){return"string"==typeof r?Array.from(document.querySelectorAll(r)):Array.isArray(r)?r:"length"in r?Array.from(r).reduce(e,[]):[r]},e=function(r,e){return e.nodeType===Node.ELEMENT_NODE&&r.push(e),r};export{r as accept};
-//# sourceMappingURL=index.module.js.map
